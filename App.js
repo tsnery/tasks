e953875d1a65d0bbfbd9e53eba8f72151aa86493
Tainan Nery <zaoldyeck@outlook.com>
2021-02-06 09:22:54 -0300

    adicionada tela de login e o Navigator component para uma entry screen e rotas de navegaçao.

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import TaskList from './src/screens/TaskList'
+import Navigator from './src/Navigator';
 
 export default function App() {
   return (
-      <TaskList/>
+      <Navigator/>
   );
 }
